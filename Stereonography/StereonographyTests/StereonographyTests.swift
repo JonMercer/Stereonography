cb19feb5ebedb5a4db8d3c7166b0d87e245e9588
JonMercer <mercer.jon@gmail.com>
2016-02-27 17:30:37 -0800

    first commit

---
+++ Stereonography/StereonographyTests/StereonographyTests.swift
@@ -0,0 +1,36 @@
+//
+//  StereonographyTests.swift
+//  StereonographyTests
+//
+//  Created by Odin on 2016-02-27.
+//  Copyright © 2016 Purple Octopus. All rights reserved.
+//
+
+import XCTest
+@testable import Stereonography
+
+class StereonographyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
