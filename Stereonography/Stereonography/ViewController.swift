88970bb494d2f618046529ed0d7c286220ba1b9a
kgrover97 <grover.karan97@gmail.com>
2016-02-28 05:19:19 -0800

    QA insert entered

--- Stereonography/Stereonography/ViewController.swift
+++ Stereonography/Stereonography/ViewController.swift
@@ -41,13 +41,6 @@ class ViewController: UIViewController {
             
             
         }
-            
-           
-            
-        
-        
-        
-        
         
   
         
