6b96f69fd309763aca488f7351f6b7cb8a347259
JonMercer <mercer.jon@gmail.com>
2016-02-27 21:30:42 -0800

    Changed names of scenes, creates Steg Model

---
+++ Stereonography/Stereonography/StegViewController.h
@@ -0,0 +1,10 @@
+//
+//  ViewController.h
+//  dataModel
+//
+//  Created by omar abo baker on 2016-02-27.
+//  Copyright © 2016 omar Abu-Baker. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+@interface StegViewController : UIViewController
+@end
